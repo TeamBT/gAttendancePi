b55a54e2c8849e6e5b01578bedac96e8c9bf9a89
Cody Duskin <codyduskin@me.com>
2017-09-19 11:50:24 -0600

    ajax

--- app.js
+++ app.js
@@ -1,18 +1,48 @@
 $(document).ready(function() {
-
-$(document).keypress(function (e) {
+const putURL = 'https://arcane-spire-19269.herokuapp.com/student/update?rfid='
+  $(document).keypress(function(e) {
     if (e.which == 13) {
       e.preventDefault()
       let userRFID = $('#input').val()
+
       console.log(userRFID)
-      $('#input').val('')      
+
+      // $.ajax({
+      //   url: URL + userRFID,
+      //   type: 'PUT',
+      //   data: {},
+      //   dataType: 'json',
+      //   success: function(data) {
+      //      $('#input').val('')
+      //   },
+      //   error: function(err) {
+      //     alert(err)
+      //   },
+      // })
+      $('#input').val('')
     }
-});
-  $('.submit').click(function (event){
+  });
+
+
+  $('.submit').click(function(event) {
+    e.preventDefault()
     let userRFID = $('#input').val()
+
     console.log(userRFID)
+
+    $('#input').val('')
+    // $.ajax({
+    //   url: URL + userRFID,
+    //   type: 'PUT',
+    //   data: {},
+    //   dataType: 'json',
+    //   success: function(data) {
+    //      $('#input').val('')
+    //   },
+    //   error: function(err) {
+    //     alert(err)
+    //   },
+    // })
+    $('#input').val('')
   })
 })
-
-
-
