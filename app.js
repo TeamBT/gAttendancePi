d65cc8cb94d35139eca2814b9be2dcf2cf2d0a22
Cody Duskin <codyduskin@me.com>
2017-09-20 09:57:21 -0600

    put

--- app.js
+++ app.js
@@ -1,48 +1,47 @@
 $(document).ready(function() {
-const putURL = 'https://arcane-spire-19269.herokuapp.com/student/update?rfid='
-  $(document).keypress(function(e) {
-    if (e.which == 13) {
-      e.preventDefault()
+  const URL = 'https://cors-anywhere.herokuapp.com/https://arcane-spire-19269.herokuapp.com/student/update?rfid='
+  $(document).keypress(function(event) {
+    if (event.which == 13) {
+      event.preventDefault()
       let userRFID = $('#input').val()
 
       console.log(userRFID)
 
-      // $.ajax({
-      //   url: URL + userRFID,
-      //   type: 'PUT',
-      //   data: {},
-      //   dataType: 'json',
-      //   success: function(data) {
-      //      $('#input').val('')
-      //   },
-      //   error: function(err) {
-      //     alert(err)
-      //   },
-      // })
+      $.ajax({
+        url: URL + userRFID,
+        type: 'PUT',
+        data: {},
+        dataType: 'json',
+        success: function(data) {
+          $('#input').val('')
+        },
+        error: function(err) {
+          console.log(err);
+        }
+      })
       $('#input').val('')
     }
   });
 
-
   $('.submit').click(function(event) {
-    e.preventDefault()
+    event.preventDefault()
     let userRFID = $('#input').val()
 
     console.log(userRFID)
 
     $('#input').val('')
-    // $.ajax({
-    //   url: URL + userRFID,
-    //   type: 'PUT',
-    //   data: {},
-    //   dataType: 'json',
-    //   success: function(data) {
-    //      $('#input').val('')
-    //   },
-    //   error: function(err) {
-    //     alert(err)
-    //   },
-    // })
+    $.ajax({
+      url: URL + userRFID,
+      type: 'PUT',
+      data: {},
+      dataType: 'json',
+      success: function(data) {
+        $('#input').val('')
+      },
+      error: function(err) {
+        console.log(err);
+      }
+    })
     $('#input').val('')
   })
 })
